90fee30d9876901d040cfdc8e9f9bf99d7cf3ef8
Brad Wilcox <brad_wilcox@me.com>
2013-08-03 10:06:08 -0400

    initial commit

---
+++ foundation/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foundation::Application.config.secret_key_base = '68795c90cf22f45a8addd98fccec952f9bfde2085ea361b5fc31cbb3aa4aee45e94ff7b3422905a2fb204be70f8d97da1e1c1f1eabd06fef00446dff58081029'
